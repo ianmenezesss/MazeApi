57d3b71751af2517d961d07936528a6360927ecb
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2025-05-12 21:39:23 -0300

    Adicionando o front

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import scoreRouter from './routes/scoreboard.js';
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5173/'],
+  origin: ['http://localhost:5173', 'http://localhost:5173/, https://maze-phi-six.vercel.app/'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 };
